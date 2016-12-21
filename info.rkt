91e181b00e09ea44dd6cb89949e13712c73690fb
Georges Dupéron <jahvascriptmaniac+github@gmail.com>
2016-12-21 06:30:33 +0100

    Added missing info.rkt file
    `raco setup` gives an error for this package otherwise

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "rash")
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '())
+(define pkg-desc "A *nix shell written in Racket")
+(define version "0.0")
+(define pkg-authors '("Cristian Esquivias"))
